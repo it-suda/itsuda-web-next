4a272d582d8fcefb4b32ce86a0e00297853b7790
Jess2 <jess2.developer@gmail.com>
2022-11-12 19:13:27 +0900

    메인 페이지 배너 추가

--- components/banner/index.tsx
+++ components/banner/index.tsx
@@ -1,11 +1,61 @@
-import styled from "styled-components";
+import banners from "data/banners";
+import { useEffect, useState } from "react";
+import S from "./style";
 
 export default function Banner() {
-  return <S.Banner>Banner</S.Banner>;
-}
+  const intervalTime: number = 2000;
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
 
-const S = {
-  Banner: styled.div`
-    height: 100vh;
-  `,
-};
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (currentIndex >= banners.length - 1) {
+        setCurrentIndex(0);
+      } else {
+        setCurrentIndex((currentIndex) => currentIndex + 1);
+      }
+    }, intervalTime);
+    return () => clearInterval(timer);
+  }, [currentIndex]);
+
+  const getState = (i: number) => {
+    if (i === currentIndex) {
+      return "current";
+    } else if (
+      i === currentIndex + 1 ||
+      (currentIndex === banners.length - 1 && i === 0)
+    ) {
+      return "next";
+    } else if (
+      i === currentIndex - 1 ||
+      (currentIndex === 0 && i === banners.length - 1)
+    ) {
+      return "prev";
+    } else {
+      return "hidden";
+    }
+  };
+
+  return (
+    <S.Container>
+      <S.Banner
+        image={banners[currentIndex].image.src}
+        bgColor={banners[currentIndex].bgColor}
+      >
+        <S.Contents>
+          잇수다에는
+          <p>
+            <S.Box>
+              {banners.map(({ name }, index) => (
+                <S.Name key={index} state={getState(index)}>
+                  {name}
+                </S.Name>
+              ))}
+            </S.Box>
+            를 위한
+          </p>
+          이벤트 잇수다
+        </S.Contents>
+      </S.Banner>
+    </S.Container>
+  );
+}
--- components/events/index.tsx
+++ components/events/index.tsx
@@ -1,3 +1,16 @@
+import Container from "components/common/Container";
+import styled from "styled-components";
+
 export default function Events() {
-  return <>Events</>;
+  return (
+    <S.Event>
+      <Container>Events</Container>
+    </S.Event>
+  );
 }
+
+const S = {
+  Event: styled.div`
+    min-height: 100vh;
+  `,
+};
